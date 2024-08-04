bd124b4d50f8e485d091464128faa9200d70428a
anvarwouldnever <140970493+anvarwouldnever@users.noreply.github.com>
2024-08-04 17:46:35 +0500

    Update ToDoItem.js

--- components/ToDoItem.js
+++ components/ToDoItem.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
-
-const SCREEN_WIDTH = Dimensions.get('window').width
-const SCREEN_HEIGHT = Dimensions.get('window').height
+import { View, Text, TouchableOpacity } from 'react-native';
+import styles from '../styles/stylesItem';
 
 const ToDoItem = ({ item, onDelete, onDone }) => {
 
@@ -22,42 +20,5 @@ const ToDoItem = ({ item, onDelete, onDone }) => {
         </View>
     )
 };
-
-const styles = StyleSheet.create({
-  item: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-    marginVertical: 8,
-    borderColor: '#ccc', borderWidth: 1, borderStyle: 'solid',
-    borderRadius: 5,
-    width: SCREEN_WIDTH * 0.8905,
-    height: 'auto',
-  },
-  delete: {
-    color: 'red',
-  },
-  textView: {
-    width: SCREEN_WIDTH * 0.6870229, 
-    height: '100%', 
-    marginLeft: 10, 
-    flexDirection: 'row', 
-    alignItems: 'center'
-  },
-  buttonAndTextView: {
-    width: 'auto', height: '100%', flexDirection: 'row'
-  },
-  button: {
-    justifyContent: 'center', 
-    alignItems: 'center', 
-    width: 20, 
-    height: 20, 
-    borderRadius: 5, 
-    borderColor: '#ccc', 
-    borderWidth: 1, 
-    borderStyle: 'solid', 
-    alignSelf: 'center'
-  }
-});
 
 export default ToDoItem;
