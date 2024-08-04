ba72ecbfaf541897bb6f6ea49cbd85979bf7c842
anvarwouldnever <140970493+anvarwouldnever@users.noreply.github.com>
2024-08-04 17:45:41 +0500

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import { View, TextInput, KeyboardAvoidingView, StyleSheet, TouchableOpacity, Text, Keyboard, Dimensions, Platform } from 'react-native';
+import { View, TextInput, KeyboardAvoidingView, TouchableOpacity, Text, Keyboard, Platform } from 'react-native';
 import ToDoList from '../components/ToDoList';
 import useToDo from '../hooks/useToDo';
-
-const SCREEN_WIDTH = Dimensions.get('window').width
-const SCREEN_HEIGHT = Dimensions.get('window').height
+import styles from '../styles/stylesHome';
 
 const HomeScreen = () => {
   const [text, setText] = useState('');
@@ -41,62 +39,5 @@ const HomeScreen = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  input: {
-    borderColor: 'black', borderWidth: 1, borderStyle: 'solid',
-    width: SCREEN_WIDTH * 0.71246,
-    height: SCREEN_HEIGHT * 0.07042,
-    borderRadius: 50,
-    paddingHorizontal: 15
-  },
-  addbutton: {
-    width: SCREEN_WIDTH * 0.152671,
-    height: SCREEN_HEIGHT * 0.07042,
-    borderRadius: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  headerView: {
-    position: 'absolute', 
-    left: SCREEN_WIDTH * 0.05089, 
-    top: 80
-  },
-  headerText: {
-    color: 'black', 
-    fontSize: 30, 
-    fontWeight: '600'
-  },
-  line: {
-    width: SCREEN_WIDTH * 0.8905, 
-    borderColor: '#D6D6D6', 
-    borderWidth: 0.6, 
-    borderStyle: 'solid', 
-    marginTop: 10
-  },
-  flatlist: {
-    position: 'absolute', 
-    top: 140, 
-    height: SCREEN_HEIGHT * 0.68075, 
-    width: SCREEN_WIDTH * 0.8905, 
-    left: SCREEN_WIDTH * 0.05089
-  },
-  inputView: {
-    position: 'absolute', 
-    width: SCREEN_WIDTH * 0.89058, 
-    flexDirection: 'row', 
-    bottom: 30, 
-    right: SCREEN_WIDTH * 0.05089, 
-    justifyContent: 'space-between',
-    backgroundColor: 'white'
-  },
-  plus: {
-    fontSize: 30, 
-    lineHeight: 30
-  }
-});
 
 export default HomeScreen;
