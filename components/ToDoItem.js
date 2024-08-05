744f3054af31975136d51185788c2dc12363ad4e
anvarwouldnever <140970493+anvarwouldnever@users.noreply.github.com>
2024-08-05 10:30:02 +0500

    Update ToDoItem.js

--- components/ToDoItem.js
+++ components/ToDoItem.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import styles from '../styles/stylesItem';
+import { useDeleteAnimation } from '../animations/animation';
+import Animated from 'react-native-reanimated';
 
 const ToDoItem = ({ item, onDelete, onDone }) => {
+    const [isDeleting, setIsDeleting] = useState(false);
+    const animatedDeletion = useDeleteAnimation(isDeleting);
+
+    const handleDelete = () => {
+      setIsDeleting(true);
+      setTimeout(() => {
+        onDelete(item.id);
+      }, 400);
+    };
 
     return (
-        <View style={styles.item}>
+        <Animated.View style={[styles.item, animatedDeletion]}>
             <View style={styles.buttonAndTextView}>
                 <TouchableOpacity onPress={() => onDone(item.id)} style={styles.button}>
                     {item.done && <Text>✓</Text>}
@@ -14,10 +25,10 @@ const ToDoItem = ({ item, onDelete, onDone }) => {
                     <Text style={{textDecorationLine: item.done? 'line-through' : 'none'}}>{item.text}</Text>
                 </View>
             </View>
-            <TouchableOpacity style={{alignSelf: 'center'}} onPress={() => onDelete(item.id)}>
+            <TouchableOpacity style={{alignSelf: 'center'}} onPress={handleDelete}>
               <Text style={styles.delete}>X</Text>
             </TouchableOpacity>
-        </View>
+        </Animated.View>
     )
 };
 
